751bea964a34104dc4192f38d585d1791e76a018
Nafiz Islam <nafiz.islam1000@gmail.com>
2026-09-04 20:42:55 -0400

    handle update also

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ function submissionMain() {
   /**
    * @type {HTMLFormElement | null}
    */
-  const form = document.getElementById("submission-form");
+  const form = document.getElementById("index-form");
   const status = document.getElementById("submission-status");
   if (!form || !status) return;
 
@@ -32,3 +32,36 @@ function submissionMain() {
 }
 
 submissionMain()
+
+function updateMain() {
+  /**
+   * @type {HTMLFormElement | null}
+   */
+  const form = document.getElementById("update-form");
+  const status = document.getElementById("submission-status");
+  if (!form || !status) return;
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+      });
+
+      if (!response.ok) {
+        alert(JSON.stringify(await response.json(), null, 2));
+      }
+
+      status.innerText = response.ok
+        ? "Successfully updated entry."
+        : "Failed to update entry.";
+    } catch (error) {
+      alert(error);
+      status.innerText = "Failed to update entry.";
+    }
+  });
+}
+
+updateMain()
